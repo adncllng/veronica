a41cac0f88d02f8b38a9cf201cd25a43788094bc
charlotteeed <c.diondufourd@live.ca>
2017-08-23 14:44:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 400); 
+    setTimeout(carousel, 300); 
 }
 
                        
