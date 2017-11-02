be6bd7a169e583039300aca30e35a0dce3bb2deb
VeronicaFM <31071240+VeronicaFM@users.noreply.github.com>
2017-11-02 10:45:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 150); 
+    setTimeout(carousel, 250); 
 }
 
                        
